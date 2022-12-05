579927a5c5488f56cde5d6a11bfdfa89e857299c
wobsoriano <sorianorobertc@gmail.com>
2022-12-05 00:54:52 -0800

    install on nitro only

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -2,19 +2,7 @@ import type { Options } from './types'
 import unplugin from '.'
 
 export default function (options: Options = {}, nuxt: any) {
-  // install webpack plugin
-  nuxt.hook('webpack:config', async (config: any) => {
-    config.plugins = config.plugins || []
-    config.plugins.unshift(unplugin.webpack(options))
-  })
-
-  // install vite plugin
-  nuxt.hook('vite:extendConfig', async (config: any) => {
-    config.plugins = config.plugins || []
-    config.plugins.push(unplugin.vite(options))
-  })
-
-  // install rollup plugin for server imports
+  // for Nitro imports
   nuxt.hook('nitro:config', (config: any) => {
     config.rollupConfig = config.rollupConfig || {}
     config.rollupConfig.plugins = config.rollupConfig.plugins || []
